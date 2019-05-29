45b1b3e57d5651688f31e214884e4bd95ab5d88e
liuyang57 <liuyang57@xiaomi.com>
2019-05-29 11:05:23 +0800

    修改md

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+
+###1. 将项目克隆到本地, cd到项目根路径下然后运行npm install
+
+###2. 然后执行npm start即可启动项目
+
+-----------------
+
+
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import './components/antd'
 import AntDesignDemo from './components/antd';
+import EchartsDemo from './components/echarts'
+import { Table, Divider, Tag} from 'antd';
 
 function App() {
   return (
     <div className="App">
+      <Divider>UI框架AntDesign</Divider>
+      <a href="https://ant.design/docs/react/introduce-cn" />
       <AntDesignDemo></AntDesignDemo>
+      <Divider>图表框架Echarts</Divider>
+      <a href="https://echarts.baidu.com/examples/" />
+      <EchartsDemo />
     </div>
   );
 }
